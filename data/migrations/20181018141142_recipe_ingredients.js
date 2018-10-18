98aa93ef3907152b3772908405be7b6a22353432
crypto-jones <integral.being@gmail.com>
2018-10-18 14:20:40 -0700

    Create recipe_ingredients table

---
+++ data/migrations/20181018141142_recipe_ingredients.js
@@ -0,0 +1,18 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('recipe_ingredient', function(tbl) {
+    tbl.increments();
+
+    tbl.string('quantity').notNullable();
+
+    tbl
+      .integer('recipe_id')
+      .notNullable()
+      .unsigned()
+      .references('id')
+      .inTable('ingredients');
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('recipe_ingredient');
+};
